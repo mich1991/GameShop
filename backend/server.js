103383d644dc1164126b77df2c309165a2df308d
Michal Wozniak <wozniak.michal91@gmail.com>
2020-12-04 12:26:42 +0000

    basic backend done. Back to Front

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const products = require('./data/products')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Api is running')
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(product => product._id === req.params.id)
+
+    res.json(product)
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log('Server Running on port 5000'))
