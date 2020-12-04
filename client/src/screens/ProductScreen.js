3b1ca268feac4c8f71de651b138b644fd346bba9
Michal Wozniak <wozniak.michal91@gmail.com>
2020-12-04 11:58:02 +0000

    Products display done. Time to build backend

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -9,7 +9,53 @@ const Product = ({ match }) => {
 
     return (
         <>
-            <Link to='/' className='btn btn-dark my-3' >Go Back</Link>
+            <Link to='/' className='btn btn-warning my-3' >Go Back</Link>
+            <Row>
+                <Col md={6}>
+                    <Image src={product.image} alt={product.name} fluid />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <h3>{product.name}</h3>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <Rating rate={product.rating} reviews={product.numReviews} />
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <h4><strong> Price:</strong> {product.price} $</h4>
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <p><strong> Description:</strong> {product.description}</p>
+                        </ListGroup.Item>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Price:</Col>
+                                    <Col><strong>{product.price}$</strong></Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Status:</Col>
+                                    <Col>
+                                        {product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Button className='btn-block btn-success' type='button' disabled={product.countInStock <= 0} >
+                                    Add To Cart
+                                </Button>
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
         </>
     )
 }
