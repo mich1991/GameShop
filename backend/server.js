57ba1c830ab6264db350b50ef4c50562d48df87d
Michal Wozniak <wozniak.michal91@gmail.com>
2020-12-07 14:00:54 +0000

    errorMiddleware and creating redux store

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import products from './data/products.js'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import productRoutes from './routes/productRoutes.js'
+
 dotenv.config()
 
 connectDB()
@@ -13,15 +15,11 @@ app.get('/', (req, res) => {
     res.send('Api is running')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(product => product._id === req.params.id)
+app.use(notFound)
+app.use(errorHandler)
 
-    res.json(product)
-})
 
 const PORT = process.env.PORT || 5000
 
