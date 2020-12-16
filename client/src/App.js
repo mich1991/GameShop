9e0fd4c658a62c30edf27400fd0cfc53e0649462
Michal Wozniak <wozniak.michal91@gmail.com>
2020-12-16 15:30:37 +0000

    Login screen & form finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+import LoginScreen from './screens/LoginScreen'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import { Container } from 'react-bootstrap'
 
 function App() {
   return (
@@ -12,9 +13,10 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/' component={HomeScreen} exact />
+          <Route path='/login' component={LoginScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
