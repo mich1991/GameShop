3d22ecb696f56b0e2c32756fb28336b8b448fa92
Michal Wozniak <wozniak.michal91@gmail.com>
2020-12-23 15:47:43 +0000

    After logout state reset added

--- client/src/reducers/orderReducers.js
+++ client/src/reducers/orderReducers.js
@@ -8,10 +8,11 @@ import {
     ORDER_LIST_MY_FAIL,
     ORDER_LIST_MY_REQUEST,
     ORDER_LIST_MY_SUCCESS,
+    ORDER_LIST_MY_RESET,
     ORDER_PAY_FAIL,
     ORDER_PAY_REQUEST,
     ORDER_PAY_RESET,
-    ORDER_PAY_SUCCESS
+    ORDER_PAY_SUCCESS,
 } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
@@ -106,6 +107,8 @@ export const orderListMyReducer = (state = { orders: [] }, action) => {
                 loading: false,
                 error: payload
             }
+        case ORDER_LIST_MY_RESET:
+            return { orders: [] }
         default:
             return state
     }
