7e3639c3c145628f2eab58911c3f234524d7f02e
Michal Wozniak <wozniak.michal91@gmail.com>
2020-12-23 16:02:17 +0000

    admin auth check added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,9 @@
 import express, { Router } from 'express'
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userControllers.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers } from '../controllers/userControllers.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = Router()
-
+router.route('/').get(protect, admin, getUsers)
 router.route('/').post(registerUser)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
