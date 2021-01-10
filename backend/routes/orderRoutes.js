d854e0a2870c13e923c84538a275b86321886b5a
Michal Wozniak <wozniak.michal91@gmail.com>
2021-01-10 16:42:21 +0000

    uploading images added

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,10 +1,10 @@
 import express, { Router } from 'express'
-import { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders } from '../controllers/orderControllers.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders, getOrders } from '../controllers/orderControllers.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = Router()
 
-router.route('/').post(protect, addOrderItems)
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders)
 router.route('/myorders').get(protect, getMyOrders)
 router.route('/:id').get(protect, getOrderById)
 router.route('/:id/pay').put(protect, updateOrderToPaid)
